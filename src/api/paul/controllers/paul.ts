06592326fac080e31cc6d8e411556a59b133365c
paulsingh-1502 <paulsingharon@gmail.com>
2025-09-18 22:14:08 +0530

    Initial commit with Strapi setup

---
+++ src/api/paul/controllers/paul.ts
@@ -0,0 +1,7 @@
+/**
+ * paul controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::paul.paul');
---
+++ src/api/paul/routes/paul.ts
@@ -0,0 +1,7 @@
+/**
+ * paul router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::paul.paul');
